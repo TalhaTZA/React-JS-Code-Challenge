53c8e1e400bbf208ed85944fde3cab1f3e2052b7
TalhaTZA <33027759+TalhaTZA@users.noreply.github.com>
2019-06-11 21:04:01 +0500

    initial commit and ADD react-router to handle different routes

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import App from "./App";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Redirect to="/" />
+        </Switch>
+      </Fragment>
+    );
+  }
+}
